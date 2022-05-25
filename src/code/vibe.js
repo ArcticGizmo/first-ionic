31a0396965279e90189d9271e880eb7a9d37f7f1
Jon <jon.howell314@gmail.com>
2022-05-25 19:11:10 +0800

    💄 added shake to web versions (nicer visual)

--- src/code/vibe.js
+++ src/code/vibe.js
@@ -1,9 +1,29 @@
 import { Haptics } from '@capacitor/haptics';
 
+import { isPlatform } from '@ionic/vue';
+
+class VibeWeb {
+  constructor() {
+    this._timeout = null;
+  }
+
+  trigger() {
+    const doc = document.documentElement;
+
+    this._timeout = clearTimeout(this._timeout);
+    if (!doc.classList.contains('shake')) {
+      doc.classList.add('shake');
+    }
+    this._timeout = setTimeout(() => doc.classList.remove('shake'), 100);
+  }
+}
+
 class Vibe {
   trigger() {
     Haptics.vibrate();
   }
 }
 
-export default new Vibe();
+const vibe = isPlatform('desktop') ? new VibeWeb() : Vibe();
+
+export default vibe;
